749eb871b43cd57cef10058bbbe5febb28fb2f33
Isa Milefchik <isamilefchik@outlook.com>
2016-06-23 20:45:46 -0700

    php file

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href='https://fonts.googleapis.com/css?family=PT+Serif' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Cinzel' rel='stylesheet' type='text/css'>
+    <title>Riddles</title>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css"
+</head>
+<body style="margin: 5;">
+<div class="logo">
+    <header>Enigmatic Mathematics</header>
+</div>
+<nav class="navBar">
+    <ul>
+        <li>Home</li>
+        <li>Log In</li>
+        <li>Sign Up</li>
+        <li>Riddles</li>
+        <li>Contact</li>
+    </ul>
+    <div> &#9776 </div>
+</nav>
+<main class="mainBody">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores consectetur et ipsum iusto labore magni neque nesciunt similique sint tempora, tempore, totam vel. Ipsam iste magni nisi quae voluptate.
+    </p>
+    <?php
+        echo "I am a god."
+    ?>
+</main>
+</body>
+</html>
