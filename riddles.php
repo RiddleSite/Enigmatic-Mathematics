56b40855d09ecec9834c99c8860bc1be6d3107ed
Isa Milefchik <isamilefchik@outlook.com>
2016-08-05 20:37:33 -0700

    Actually added riddles.php

---
+++ riddles.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href='https://fonts.googleapis.com/css?family=PT+Serif' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Cinzel' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <script src="web_effects.js"></script>
+    <script type="text/x-mathjax-config">
+  MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});
+    </script>
+    <script src='https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'></script>
+    <link rel="shortcut icon" href="favicon.ico" type="favicon/ico">
+
+    <title>Enigmatic Mathematics</title>
+
+</head>
+<body>
+<header class="topBody">
+    <a href="index.php">
+        <div class="logo">
+            <header>Enigmatic Mathematics</header>
+        </div>
+    </a>
+    <nav id="navBar">
+        <ul>
+            <li>
+                <a href="index.php" style="text-decoration: none;">Home</a>
+            </li>
+
+            <li>
+                <a href="riddles.php">Riddles</a>
+            </li>
+
+            <li>
+                <a href="login.php" style="text-decoration: none;">Log In</a>
+            </li>
+
+            <li>
+                <a href="signUp.php">Sign Up</a>
+            </li>
+
+            <li>
+                <a href="contact.php">Contact</a>
+            </li>
+        </ul>
+    </nav>
+</header>
